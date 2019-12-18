c665aa0edc1bd7c0c22c8662c9b379f72674132e
brunoczim <brunoczim@gmail.com>
2019-12-18 12:15:04 -0300

    idea for a new incinerator, now with notion of epoch, also no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,348 @@
-#![warn(missing_docs)]
-//! A crate providing lock-free data structures and a solution for the "ABA
-//! problem" related to pointers.
-//!
-//! The incinerator is the API which tries to solve the "ABA problem" when
-//! related to pointer dropping. With incinerator, every thread has a local
-//! garbage list. Dropping a shared object consist of first removing the pointer
-//! from the shared context, then adding the pointer to the garbage list.
-//! A "pause counter" is checked. If the counter is zero, then the whole list
-//! is deleted, otherwise, the list will only be deleted later.
-//!
-//! This counter is counting how many times the incinerator was asked to
-//! "pause". A thread may pause the incinerator to load and use the shared
-//! pointer, and this is why it is important to remove the pointer from the
-//! shared context before deleting. Previous version of lockfree used a global
-//! incinerator. Currently, a per-object incinerator is used.
-//!
-//! This crate is under development, and there are plans for some structures.
-//! We have:
-//! - `[x]` [Per-Object Thread-Local Storage](tls::ThreadLocal)
-//! - `[x]` [Channels (SPSC, MPSC, SPMC, MPMC)](channel)
-//! - `[x]` [Map](map::Map)
-//! - `[x]` [Set](set::Set)
-//! - `[x]` [Stack](stack::Stack)
-//! - `[x]` [Queue](queue::Queue)
-//! - `[ ]` Deque
-//!
-//! # Performance Guide
-//! In order to achieve a better time performance with lockfree, it is
-//! recommended to avoid global locking stuff like heap allocation.
+#![cfg_attr(not(feature = "std"), no_std)]
 
-extern crate owned_alloc;
+use core::{
+    cell::UnsafeCell,
+    mem::{size_of, MaybeUninit},
+    ops::{Deref, DerefMut},
+    sync::atomic::{fence, AtomicBool, AtomicUsize, Ordering::*},
+};
 
-/// Provides convenient re-exports.
-pub mod prelude;
+#[cfg(feature = "std")]
+use std::sync::Arc;
 
-/// Incinerator API. The purpouse of this module is to solve the "ABA problem"
-/// related to pointers while still being lock-free. See documentation of the
-/// inner type for more details.
-#[macro_use]
-pub mod incin;
+const USIZE_BITS: usize = size_of::<usize>() * 8;
+const USIZE_HIGHEST_BIT_MASK: usize = 1 << (USIZE_BITS - 1);
+const USIZE_LOWER_BITS_MASK: usize = (1 << (USIZE_BITS - 1)) - 1;
 
-/// A wait-free per-object Thread Local Storage (TLS).
-pub mod tls;
+const fn data_bits(bits: usize) -> usize {
+    bits & USIZE_LOWER_BITS_MASK
+}
 
-/// A lock-free queue.
-pub mod queue;
+const fn epoch_bit(bits: usize) -> usize {
+    bits >> (USIZE_BITS - 1)
+}
 
-/// A lock-free stack.
-pub mod stack;
+const fn set_data_bits(bits: usize, data_bits: usize) -> usize {
+    (bits & USIZE_HIGHEST_BIT_MASK) | (data_bits & USIZE_LOWER_BITS_MASK)
+}
 
-/// A lock-free map.
-pub mod map;
+const fn set_epoch_bit(bits: usize, epoch_bit: usize) -> usize {
+    (bits & USIZE_LOWER_BITS_MASK) | (epoch_bit << (USIZE_BITS - 1))
+}
 
-/// A lock-free set.
-pub mod set;
+const NULL_IDX: usize = !0;
 
-/// Collection of lock-free FIFO channels. These channels are fully asynchronous
-/// and their receivers do not provide any sort of `wait-for-message` operation.
-/// It would be blocking otherwise, thus not lock-free. If you need such a
-/// mechanism, consider using this channel with a
-/// [`Condvar`](std::sync::Condvar) or using things like
-/// [`thread::park`](std::thread::park) (not lock-free).
-pub mod channel;
+/// Initializes a slab array.
+macro_rules! make_slab_array {
+    ($size:expr) => {
+        unsafe {
+            use core::mem::{transmute, MaybeUninit};
 
-/// A shared removable value. No extra allocation is necessary.
-pub mod removable;
+            let mut arr = MaybeUninit::<[MaybeUninit<_>; $size]>::uninit();
 
-#[allow(dead_code)]
-mod ptr;
+            for elem in &mut arr[..] {
+                *elem = MaybeUninit::new(SlabBlock::new());
+            }
+
+            transmute::<_, [_; $size]>(arr)
+        }
+    };
+}
+
+/// Returned if out of memory.
+#[derive(Debug, Clone, Copy)]
+pub struct GenericAllocErr;
+
+/// A block on the slab used to allocate memory.
+#[derive(Debug)]
+pub struct SlabBlock<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    next: AtomicUsize,
+}
+
+impl<T> SlabBlock<T> {
+    /// Initializes a blank slab.
+    pub fn new() -> Self {
+        Self {
+            value: UnsafeCell::new(unsafe { MaybeUninit::uninit() }),
+            next: AtomicUsize::new(0),
+        }
+    }
+}
+
+/// The type of a slice used by the slab allocator.
+pub type SlabSlice<S> = [SlabBlock<<S as SlabStorage>::Data>];
+
+/// A device for storage of a slab, such as an array on the stack, or a
+/// heap-allocated vector.
+pub trait SlabStorage: DerefMut<Target = SlabSlice<Self>> {
+    /// Dataent that this storage can hold.
+    type Data;
+}
+
+/// A shared pointer data type.
+pub trait SharedPtr: Deref + Clone {}
+
+impl<T> SharedPtr for T where T: Deref + Clone {}
+
+/// A slab allocator.
+#[derive(Debug)]
+struct Slab<S>
+where
+    S: SlabStorage,
+{
+    /// List of nodes to be destroyed. Ended by NULL_IDX.
+    destroy_list: AtomicUsize,
+    /// List of nodes that are free to be allocated. Ended by NULL_IDX.
+    free_list: AtomicUsize,
+    /// The storage device.
+    storage: S,
+}
+
+/// Beginning and end of a freed destroy list.
+#[derive(Debug, Clone, Copy)]
+struct DestroyList {
+    /// Node beginning (included)
+    begin: usize,
+    /// Node ending (included)
+    end: usize,
+}
+
+impl<S> Slab<S>
+where
+    S: SlabStorage,
+{
+    /// Creates a new slab allocator, putting the correct indices on the blocks.
+    fn new(storage: S) -> Self {
+        let this = Self {
+            destroy_list: AtomicUsize::new(NULL_IDX),
+            free_list: AtomicUsize::new(0),
+            storage,
+        };
+
+        for (i, elem) in this.storage[..].iter().enumerate() {
+            let index =
+                if i + 1 < this.storage.len() { i + 1 } else { NULL_IDX };
+            elem.next.store(index, Release);
+        }
+
+        this
+    }
+
+    /// Actually destroys the destroy list, but does not put the freed nodes in
+    /// the free list. Returns the first and the last element of the list.
+    /// Should only be called inside destroy and inside drop.
+    fn free_destroy_list(&self, expected: usize) -> Result<DestroyList, usize> {
+        if expected == NULL_IDX {
+            return Err(expected);
+        }
+
+        let result = self
+            .destroy_list
+            .compare_exchange(expected, NULL_IDX, Release, Acquire);
+
+        result.map(|_| {
+            let begin = data_bits(expected);
+            let mut curr = begin;
+            let mut next = begin;
+
+            while next != NULL_IDX {
+                unsafe {
+                    let cell = self.storage[next].value.get();
+                    (*cell).as_mut_ptr().drop_in_place();
+                }
+                curr = next;
+                next = self.storage[curr].next.load(Relaxed);
+            }
+
+            DestroyList { begin, end: curr }
+        })
+    }
+
+    /// Destroy the free list and puts the new free nodes in the free list.
+    /// Should Only be called if no one is allocating.
+    fn destroy(&self, expected: usize) -> Result<usize, usize> {
+        self.free_destroy_list(expected).map(|freed| {
+            let mut free = self.free_list.load(Acquire);
+
+            loop {
+                self.storage[freed.end].next.store(free, Relaxed);
+                let result = self.free_list.compare_exchange(
+                    free,
+                    freed.begin,
+                    Release,
+                    Acquire,
+                );
+
+                match result {
+                    Ok(_) => break NULL_IDX,
+                    Err(update) => free = update,
+                }
+            }
+        })
+    }
+
+    /// GenericAllocates a node. Should only be called if no one is freeing the
+    /// destroy list.
+    fn alloc(&self) -> Result<usize, GenericAllocErr> {
+        let mut free = self.free_list.load(Acquire);
+
+        loop {
+            if free == NULL_IDX {
+                break Err(GenericAllocErr);
+            }
+
+            let next = self.storage[free].next.load(Relaxed);
+            let result =
+                self.free_list.compare_exchange(free, next, Release, Acquire);
+            match result {
+                Ok(_) => break Ok(free),
+                Err(update) => free = update,
+            }
+        }
+    }
+
+    /// Put a node in the to-be-destroyed-list, for later free. The node integer
+    /// must contain epoch and index.
+    fn free(&self, node: usize) {
+        let mut destroy = self.destroy_list.load(Acquire);
+
+        loop {
+            if destroy != NULL_IDX && epoch_bit(destroy) != epoch_bit(node) {
+                destroy = self
+                    .destroy(destroy)
+                    .unwrap_or_else(|new_destroy| new_destroy);
+            }
+
+            self.storage[data_bits(node)]
+                .next
+                .store(data_bits(destroy), Relaxed);
+
+            let result = self
+                .destroy_list
+                .compare_exchange(destroy, node, Release, Acquire);
+            match result {
+                Ok(_) => break,
+                Err(update) => destroy = update,
+            }
+        }
+    }
+}
+
+impl<S> Drop for Slab<S>
+where
+    S: SlabStorage,
+{
+    fn drop(&mut self) {
+        let mut curr = data_bits(*self.destroy_list.get_mut());
+
+        while curr != NULL_IDX {
+            unsafe {
+                let cell = self.storage[curr].value.get();
+                (*cell).as_mut_ptr().drop_in_place();
+            }
+            curr = data_bits(*self.storage[curr].next.get_mut());
+        }
+    }
+}
+
+/// Inner structure of collector, accessed through a pointer.
+#[derive(Debug)]
+pub struct CollectorInner<S>
+where
+    S: SlabStorage,
+{
+    slab: Slab<S>,
+    threads_epoch: AtomicUsize,
+}
+
+impl<S> CollectorInner<S>
+where
+    S: SlabStorage,
+{
+    /// Creates a new initialized inner structure of a collector.
+    pub fn new(array: S) -> Self {
+        Self { slab: Slab::new(array), threads_epoch: AtomicUsize::new(0) }
+    }
+
+    fn pause(&self) -> Pause<S> {
+        let mut bits = self.threads_epoch.load(Acquire);
+        loop {
+            let max = data_bits(usize::max_value());
+            if data_bits(bits) == max {
+                panic!("Maximum thread number reached!");
+            }
+            let new_bits = set_data_bits(bits, data_bits(bits) + 1);
+            let result = self
+                .threads_epoch
+                .compare_exchange(bits, new_bits, Release, Acquire);
+            match result {
+                Ok(_) => break,
+                Err(update) => bits = update,
+            }
+        }
+
+        Pause { collector: self }
+    }
+}
+
+/// A collector. Manages concurrent memory.
+#[derive(Debug)]
+pub struct GenericCollector<S, P>
+where
+    S: SlabStorage,
+    P: SharedPtr<Target = CollectorInner<S>>,
+{
+    inner: P,
+}
+
+impl<S, P> GenericCollector<S, P>
+where
+    S: SlabStorage,
+    P: SharedPtr<Target = CollectorInner<S>>,
+{
+    /// Creates a new collector from a given shared pointer to a inner collector
+    /// strcuture.
+    pub fn new(inner: P) -> Self {
+        Self { inner }
+    }
+
+    /// GenericAllocates memory, returning error if no space is available.
+    pub fn alloc(
+        &self,
+        val: S::Data,
+    ) -> Result<GenericAlloc<S, P>, GenericAllocErr> {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+struct Pause<'col, S>
+where
+    S: SlabStorage,
+{
+    collector: &'col CollectorInner<S>,
+}
+
+/// An allocation of memory.
+pub struct GenericAlloc<S, P>
+where
+    S: SlabStorage,
+    P: SharedPtr<Target = CollectorInner<S>>,
+{
+    collector: P,
+}
+
+/// A collector with the default pointer as Arc.
+#[cfg(feature = "std")]
+pub type Collector<S> = GenericCollector<S, Arc<CollectorInner<S>>>;
+#[cfg(feature = "std")]
+/// An alloc with the default pointer to collector as Arc.
+pub type Alloc<S> = GenericAlloc<S, Arc<CollectorInner<S>>>;
+
+#[cfg(not(feature = "std"))]
+/// A collector with the default pointer as a plain reference.
+type Collector<'inner, S> = GenericCollector<S, &'inner CollectorInner<S>>;
+#[cfg(not(feature = "std"))]
+/// An alloc with the default pointer to collector as a plain reference.
+type Alloc<'inner, S> = GenericAlloc<S, &'inner CollectorInner<S>>;
